4b4dc7cc3fc3b8b1e3cb25a0ba5dbd1b881a1e9e
Younus9096 <younus.mohd9097@gmail.com>
2025-01-14 21:51:50 +0530

    add allow host

--- cadecs/cadecs/settings.py
+++ cadecs/cadecs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6#jkb^!0ln-njw-@1-*+cy#$w#i&(v!v(jwc5sdsuw5zk3$cad
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1']
 
 
 # Application definition

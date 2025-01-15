d95db600afe1ede805a80fa70481a0b8b5d16c5f
Younus9096 <younus.mohd9097@gmail.com>
2025-01-15 21:27:16 +0530

    add apis

--- cadecs/account/views.py
+++ cadecs/account/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+
+class DashboardView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        table_lists = []                 
+        resp = {
+            'results': table_lists,            
+            'resultCode': '1',
+            'resultDescription': 'Dashboard View'
+        }
+        return Response(resp, status=status.HTTP_200_OK)
--- cadecs/cadecs/settings.py
+++ cadecs/cadecs/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'account'
 ]
 
 MIDDLEWARE = [
@@ -78,6 +81,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 

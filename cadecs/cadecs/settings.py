ce5c2b8b5f8d144cf14945a8ce85c4068606a6b8
Younus9096 <younus.mohd9097@gmail.com>
2025-01-15 22:35:29 +0530

    add cors in django

--- cadecs/cadecs/settings.py
+++ cadecs/cadecs/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'account'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
